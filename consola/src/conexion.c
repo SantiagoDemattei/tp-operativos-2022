53c491dc36f5e6277293f6c2bf76d950529ce6c8
lhouli <lhouli@frba.utn.edu.ar>
2022-04-19 20:46:20 -0300

    probando como se usa la sf

--- consola/src/conexion.c
+++ consola/src/conexion.c
@@ -12,12 +12,12 @@ int crear_conexion(t_datos_conexion* datos_conexion)
 
 	getaddrinfo(datos_conexion->ip,  datos_conexion->puerto, &hints, &server_info);
     printf("estoy conectando a la ip %s\n", datos_conexion->ip); 
-    printf("estoy conectando a el puerto %s\n", datos_conexion->puerto);
+    printf("estoy conectando al puerto %s\n", datos_conexion->puerto);
 	// Ahora vamos a crear el socket.
 	int socket_kernel = socket(server_info -> ai_family, server_info -> ai_socktype, server_info -> ai_protocol);
 	// Ahora que tenemos el socket, vamos a conectarlo
     if(connect(socket_kernel, server_info -> ai_addr, server_info -> ai_addrlen) == -1){ // aca ya deberia estar corriendo el kernel esperando conexion
-    	printf("error, no se ha encontrado conexion\n"); 
+    	printf("error, no se ha encontrado conexion :(\n"); 
     }
     else{ printf("Conectado a Kernel");}
 
